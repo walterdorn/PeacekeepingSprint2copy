b52517f0c69425dd7c27a392a78053722b7db71f
VictoriaPal <59703217+VictoriaPal@users.noreply.github.com>
2020-06-17 12:32:34 -0400

    Fungus Implimentation

---
+++ PeacekeepingSprint2/Assets/Scripts/Fungusscripts to work/DiologeEnter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiologeEnter : MonoBehaviour
+
+    
+{
+
+    public GameObject FlowChart;
+    public float enetered;
+
+    private void OnCollisionEnter(Collision hit)
+    {
+        enetered = 1;
+    }
+}
