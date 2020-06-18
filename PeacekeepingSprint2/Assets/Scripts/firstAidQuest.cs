aacf3c50aabbc8f8e1d892e95de7b91c09964901
KiazUndaunted <kiaz.undaunted@gmail.com>
2020-06-18 14:46:56 -0400

    CatherineScene and with Fungus
    Added firstAidQuest to CatherineScene and added Fungus to CatherineSceneWithFungus

---
+++ PeacekeepingSprint2/Assets/Scripts/firstAidQuest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstAidQuest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("firstAidQuest");
+
+            //Input.GetAxis and Input.GetButton (or Input.GetKeyDown)
+            if (Input.GetKey("e"))
+            {
+                Debug.Log("Got e");
+
+            }
+        }
+    }
+
+}
