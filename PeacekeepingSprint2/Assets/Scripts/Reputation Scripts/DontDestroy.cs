be2fc37a2dfbe6b6e1f1a563642f4d53a779fbfd
LVLWON <45217028+LVLWON@users.noreply.github.com>
2020-06-18 10:42:17 -0400

    Implemented DontDestroyOnLoad

---
+++ PeacekeepingSprint2/Assets/Scripts/Reputation Scripts/DontDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+
+    private static DontDestroy instance;
+
+    void Awake()
+    {
+
+        DontDestroyOnLoad(this.gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
