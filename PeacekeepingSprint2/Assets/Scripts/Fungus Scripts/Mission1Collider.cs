49312dd90ca613e1d21114fb18b34cc955b0011f
VictoriaPal <59703217+VictoriaPal@users.noreply.github.com>
2020-06-19 12:28:21 -0400

    Mission Implimented somewhat

---
+++ PeacekeepingSprint2/Assets/Scripts/Fungus Scripts/Mission1Collider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission1Collider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Fungus.Flowchart.BroadcastFungusMessage("Mission1Start");
+    }
+}
