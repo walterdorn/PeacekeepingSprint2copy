8fd473daab7921931ddf3205cfff65e2b13d8ea2
LVLWON <45217028+LVLWON@users.noreply.github.com>
2020-07-14 12:31:44 -0400

    World Manager Spawning Modifications

--- PeacekeepingSprint2/Assets/Scripts/Reputation Scripts/ReputationImpact.cs
+++ PeacekeepingSprint2/Assets/Scripts/Reputation Scripts/ReputationImpact.cs
@@ -21,31 +21,78 @@ public class ReputationImpact : MonoBehaviour
     public GameObject object5;
     public GameObject object6;
 
+    bool mapChanging = true;
+
     void Awake()
+    {
+
+        //float currentRep = GameObject.Find("ReputationBar").GetComponent<ReputationCalculation>().currentRep;
+
+        //if (currentRep <= 40)
+        //{
+
+        //    Instantiate(object1, position1.transform.position, Quaternion.identity, this.gameObject.transform);
+        //    Instantiate(object2, position2.transform.position, Quaternion.identity, this.gameObject.transform);
+        //    Instantiate(object3, position3.transform.position, Quaternion.identity, this.gameObject.transform);
+        //    Instantiate(object4, position4.transform.position, Quaternion.identity, this.gameObject.transform);
+        //    Instantiate(object5, position5.transform.position, Quaternion.identity, this.gameObject.transform);
+        //}
+
+        //else if (currentRep > 40 && currentRep < 70)
+        //{
+
+
+        //}
+
+        //else if (currentRep >= 70)
+        //{
+
+        //    Instantiate(object6, position6.transform.position, Quaternion.identity, this.gameObject.transform);
+        //}
+
+        //int childs = transform.childCount;
+        //for (int i = childs - 1; i >= 0; i--)
+        //{
+        //    GameObject.Destroy(transform.GetChild(i).gameObject);
+        //}
+    }
+
+    void Update()
     {
 
         float currentRep = GameObject.Find("ReputationBar").GetComponent<ReputationCalculation>().currentRep;
 
-        if (currentRep <= 40)
+        if (currentRep <= 40 & mapChanging == true)
         {
 
-            Instantiate(object1, position1.transform.position, Quaternion.identity);
-            Instantiate(object2, position2.transform.position, Quaternion.identity);
-            Instantiate(object3, position3.transform.position, Quaternion.identity);
-            Instantiate(object4, position4.transform.position, Quaternion.identity);
-            Instantiate(object5, position5.transform.position, Quaternion.identity);
+            Instantiate(object1, position1.transform.position, Quaternion.identity, this.gameObject.transform);
+            Instantiate(object2, position2.transform.position, Quaternion.identity, this.gameObject.transform);
+            Instantiate(object3, position3.transform.position, Quaternion.identity, this.gameObject.transform);
+            Instantiate(object4, position4.transform.position, Quaternion.identity, this.gameObject.transform);
+            Instantiate(object5, position5.transform.position, Quaternion.identity, this.gameObject.transform);
+
+            mapChanging = false;
         }
 
         else if (currentRep > 40 && currentRep < 70)
         {
 
+            int children = transform.childCount;
 
+            for (int i = children - 1; i >= 0; i--)
+            {
+                GameObject.Destroy(transform.GetChild(i).gameObject);
+            }
+
+            mapChanging = true;
         }
 
-        else if (currentRep >= 70)
+        else if (currentRep >= 70 & mapChanging == true)
         {
 
-            Instantiate(object6, position6.transform.position, Quaternion.identity);
+            Instantiate(object6, position6.transform.position, Quaternion.identity, this.gameObject.transform);
+
+            mapChanging = false;
         }
     }
 }
