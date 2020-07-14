fc1deef88cda48d1db07c2bcb8937b009effc2cd
DylanVII <48774393+DylanVII@users.noreply.github.com>
2020-07-14 11:39:16 -0400

    Added the first two dialogue in the base for the UNMO mission

---
+++ PeacekeepingSprint2/Assets/Scripts/Fungus Scripts/UNMO Mission/UNMODialogue1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UNMODialogue1 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Fungus.Flowchart.BroadcastFungusMessage("CommanderSpeaks");
+    }
+}
