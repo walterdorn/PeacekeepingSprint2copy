cc26516513dca4036f4a045cfd9503fb5b5fc898
VictoriaPal <59703217+VictoriaPal@users.noreply.github.com>
2020-07-16 10:57:57 -0400

    Village mission setup stuff

---
+++ PeacekeepingSprint2/Assets/Scripts/Fungus Scripts/VillageDiologue/VillagerDiologe2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerDiologe3 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Fungus.Flowchart.BroadcastFungusMessage("Villager3Speaking");
+    }
+}
---
+++ PeacekeepingSprint2/Assets/Scripts/Fungus Scripts/VillageDiologue/VillagerDiologe3.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerDiologe2 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Fungus.Flowchart.BroadcastFungusMessage("Villager2Speaking");
+    }
+}
