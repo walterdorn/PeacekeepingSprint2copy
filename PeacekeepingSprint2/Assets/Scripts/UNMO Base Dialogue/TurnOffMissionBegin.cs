d769dbd2c34069b318406cf9b8463bcd06b949b5
DylanVII <48774393+DylanVII@users.noreply.github.com>
2020-07-16 14:15:52 -0400

    Hit f and the dialogue option changes
    too meta

---
+++ PeacekeepingSprint2/Assets/Scripts/UNMO Base Dialogue/TurnOffMissionBegin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffMissionBegin : MonoBehaviour
+{
+
+    public GameObject BeginningCircle;
+    public GameObject SecondCircle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SecondCircle.SetActive(false);
+        BeginningCircle.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+            SecondCircle.SetActive(true);
+            BeginningCircle.SetActive(false);
+
+        }
+    }
+}
