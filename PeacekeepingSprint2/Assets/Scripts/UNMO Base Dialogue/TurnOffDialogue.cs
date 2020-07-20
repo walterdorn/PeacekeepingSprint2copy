fd9a64dac43bfc41fa47ce2282bb53941fd2f235
DylanVII <48774393+DylanVII@users.noreply.github.com>
2020-07-20 12:11:41 -0400

    Connected dialogue from base to village

--- PeacekeepingSprint2/Assets/Scripts/UNMO Base Dialogue/TurnOffDialogue.cs
+++ PeacekeepingSprint2/Assets/Scripts/UNMO Base Dialogue/TurnOffDialogue.cs
@@ -8,11 +8,15 @@ public class TurnOffDialogue : MonoBehaviour
    
     public GameObject SecondCircle;
     public GameObject VillageCircle1;
+    public GameObject VillageCircle2;
+    public GameObject VillageCircle3;
 
     // Start is called before the first frame update
     void Start()
     {
         VillageCircle1.SetActive(false);
+        VillageCircle2.SetActive(false);
+        VillageCircle3.SetActive(false);
         SecondCircle.SetActive(true);
 
     }
@@ -23,9 +27,11 @@ public class TurnOffDialogue : MonoBehaviour
     {
         if(other.tag == "Second UNMO Circle")
         {
-            if(!SecondCircle.activeSelf)
+            if(SecondCircle.activeSelf)
             {
                 VillageCircle1.SetActive(true);
+                VillageCircle2.SetActive(true);
+                VillageCircle3.SetActive(true);
                 SecondCircle.SetActive(false);
 
                 Destroy(other.gameObject);
