2a8ec152bc17e8db9f46e245f460f21438436881
David D'Orazio <daviddorazio8@gmail.com>
2020-07-20 12:13:25 -0400

    Testing UAV Things

---
+++ PeacekeepingSprint2/Assets/Scripts/FireWood/CarWaypoints.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarWaypoints : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+    private Transform targetWaypoint;
+    private int targetWaypointIndex = 0;
+    private float minDistance = 0.1f;
+    private float lastWaypointIndex;
+
+    public float movementSpeed = 0f;
+    private float rotationSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastWaypointIndex = waypoints.Count - 1;
+        //target waypoint is equal to one in the list
+        targetWaypoint = waypoints[targetWaypointIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //make speed based from time.deltatime and rotation speed
+        float movementStep = movementSpeed * Time.deltaTime;
+        float rotationStep = rotationSpeed * Time.deltaTime;
+
+        //find out how much the player needs to rotate by
+        Vector3 directionToTarget = targetWaypoint.position - transform.position;
+        Quaternion rotationToTarget = Quaternion.LookRotation(directionToTarget);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationToTarget, rotationStep);
+
+        //check distance between waypoints and moving object
+        float distance = Vector3.Distance(transform.position, targetWaypoint.position);
+        CheckDistanceToWaypoint(distance);
+        //move the player to the waypoint position
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movementStep);
+
+       
+    }
+
+    void CheckDistanceToWaypoint(float currentDistance)
+    {
+        if (currentDistance <= minDistance)
+        {
+            //increase the target waypoint index when object is close to the waypoint
+            targetWaypointIndex++;
+            UpdateTargetWaypoint();
+        }
+    }
+
+    void UpdateTargetWaypoint()
+    {
+        //rest back the loop so it keeps going around
+        if (targetWaypointIndex > lastWaypointIndex)
+        {
+            targetWaypointIndex = 0;
+        }
+        targetWaypoint = waypoints[targetWaypointIndex];
+    }
+
+    
+}
+
+
+
+
+
+
