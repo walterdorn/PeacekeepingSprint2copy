b6cce1bea5ed16126f37c48f4797b764750e9fc2
KiazUndaunted <kiaz.undaunted@gmail.com>
2020-07-23 10:59:22 -0400

    Main Scene - edited order of fungus dialogue for car crash first aid
    go from dialogue where hear about casualty to radio dialogue to get tourniquet to use tourniquet. Still need to make final casualty dialogue active on using tourniquet.

--- PeacekeepingSprint2/Assets/Mission5ColliderFirstAid.cs
+++ PeacekeepingSprint2/Assets/Mission5ColliderFirstAid.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Mission5ColliderFirstAid : MonoBehaviour
 {
-
     private void OnTriggerEnter(Collider other)
     {
         Fungus.Flowchart.BroadcastFungusMessage("Mission5Start");
+
     }
 
 }
