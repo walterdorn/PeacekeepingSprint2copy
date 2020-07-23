024a10dee47366553bae0b317dec00fe8a5d9885
VictoriaPal <59703217+VictoriaPal@users.noreply.github.com>
2020-07-23 10:58:45 -0400

    Village Part 2 Set up

---
+++ PeacekeepingSprint2/Assets/Scripts/Fungus Scripts/VillageDiologue/VillageDiologeAssets2Sentry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillageDiologeAssets2Sentry : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Fungus.Flowchart.BroadcastFungusMessage("VillageGuardSpeaking");
+    }
+}
