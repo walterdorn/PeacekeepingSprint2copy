6f41f3dd8dabff07d7429f40d1959bd8b3fd42af
LVLWON <45217028+LVLWON@users.noreply.github.com>
2020-08-04 14:21:00 -0400

    Various Updates
    - new dialogue
    - sound fixes
    - asset changes
    - NPC name changes

---
+++ PeacekeepingSprint2/Assets/Scripts/Fungus Scripts/ShepherdSpeaks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShepherdSpeaks : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Fungus.Flowchart.BroadcastFungusMessage("ShepherdSpeaks");
+    }
+
+}
