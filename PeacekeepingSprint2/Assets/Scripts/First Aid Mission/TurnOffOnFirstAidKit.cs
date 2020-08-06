f5abe73cae9bc997e595923981d4e9ff3b7349cc
DylanVII <48774393+DylanVII@users.noreply.github.com>
2020-08-06 14:17:05 -0400

    Turned off first aid kit
    turned off first aid kit in the car crash mission so you cant skip the  beginning of the mission

---
+++ PeacekeepingSprint2/Assets/Scripts/First Aid Mission/TurnOffOnFirstAidKit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffOnFirstAidKit : MonoBehaviour
+{
+
+    public GameObject FirstAidKit;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        FirstAidKit.SetActive(false);
+    }
+
+
+    void TurnMedKitOff()
+    {
+
+        FirstAidKit.SetActive(true);
+
+    }
+}
