ff9f3c5fc6e5ccc802724b866b6fa08e1ae2465d
LVLWON <45217028+LVLWON@users.noreply.github.com>
2020-08-10 11:45:40 -0400

    Added Reputation Toggle

---
+++ PeacekeepingSprint2/Assets/Scripts/Reputation/ReputationToggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReputationToggle : MonoBehaviour
+{
+
+    //reference canvas group on reputation bar
+    public CanvasGroup group;
+
+    //bool to toggle visibility
+    bool isOn = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //make canvas group transparent
+        if (Input.GetKeyDown(KeyCode.Y) && isOn == true)
+
+        {
+
+            group.alpha = 0;
+            isOn = !isOn;
+        }
+
+        //make canvas group visible
+        else if (Input.GetKeyDown(KeyCode.Y) && isOn == false)
+        {
+
+            group.alpha = 1;
+            isOn = !isOn;
+        }
+    }
+}
